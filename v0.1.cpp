4b48fd401b0fa5a21bfcc33b55156dc65ad5a1bb
Benjaminas Paliokas <76651648+alojine@users.noreply.github.com>
2022-02-10 23:35:53 +0200

    Add files via upload

--- v0.1.cpp
+++ v0.1.cpp
@@ -8,52 +8,111 @@ using std::string;
 
 struct data {
 	string vardas ="", pavarde ="";
-	int paz[50] = { 0 }, egz = 0;
-	double rezult = 0;
+	int paz[50] = { 0 }, egz = 0, n = 2;
+	double v = 0, m = 0;
 };
 
 void input(data& s);
 
+double vidurkis(data& s);
+
+double mediana(data& s);
+
 void output(data& s);
+
 
 int main() {
 	data s;
+	/*
 	data* mas = new data[3];
 	for (int i = 0; i < 3; i++) {
 		input(mas[i]);
 	}
+	*/
+	cout << "Atminkite, kad jeigu ivesite 0 tai nutrauks ivedima, ar tai butu pazymiai, ar studentai." << std::endl;
 	input(s);
+
+	s.v = vidurkis(s);
+	s.m = mediana(s);
+
 	output(s);
+
+	/*
 	for (int i = 0; i < 3; i++) {
 		output(mas[i]);
 	}
-	delete [] mas;
+	*/
+
+	//delete [] mas;
 	system("pause");
 }
 
 void input(data& s) {
-	int n = 3;
-
+	cout << std::endl;
 	cout << "Iveskite studento varda: "; cin >> s.vardas;
 	cout << "Iveskite studento pavarde: "; cin >> s.pavarde;
 
-	// cout << "Kiek pazymiu turi studentas: "; cin >> n;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < s.n; i++) {
 		cout << "Iveskite" << i + 1 << "-aji pazymi: ";
 		cin >> s.paz[i];
-		// jeigu ivedame raide, pazymiai baigiasi
+		if (s.paz[i] == 0) break;
+		s.n++;
 	}
+	s.n = s.n - 2;
 	cout << "Iveskite egzamino ivertinima: "; cin >> s.egz;
 };
 
+double vidurkis(data& s) {
+	double v = 0;
+	for (int i = 0; i < s.n; i++) {
+		v += s.paz[i];
+	}
+	v = v / s.n;
+
+	v = v * 0.4 + s.egz * 0.6;
+	return v;
+}
+
+double mediana(data& s) {
+	double m = 0;
+	int temp = 0;
+	
+	
+	for (int i = 0; i < s.n - 1; i++) {
+		temp = s.paz[i];
+		for (int j = i + 1; j < s.n; i++) {
+			if (s.paz[j] < temp) std::swap(s.paz[j],temp);
+		}
+	}
+	/*
+	
+	temp = s.n;
+	if (temp % 2 != 0) {
+		double tem = temp;
+		int mazesnis = ((double)(tem / 2) - 0.5) - 1;
+		int didesnis = ((double)(tem / 2) + 0.5) - 1;
+		m = ((s.paz[mazesnis] + s.paz[didesnis]) / 2) * 0.4 + s.egz * 0.6;
+	}
+	else {
+		m = s.paz[temp / 2] * 0.4 + s.egz * 0.6;
+	}
+	*/
+	return m;
+}
+
 void output(data& s) {
-
-	cout << "|" << std::left << std::setw(20) << "s.vardas" << std::left << std::setw(20) << s.pavarde << std::right;
-	for (int i = 0; i < 3; i++) cout << "|" << std::setw(10) << s.paz[i];
-	cout << "|" << std::setw(10) << s.egz << "|" << std::endl;
-};
-
-int vidurkis(data& s) {
+	cout << std::left << std::setw(20) << "Pavarde" << std::left << std::setw(20) << "Vardas" << std::left << std::setw(15) << "Galutinis (Vid.)"
+		<< std::left << std::setw(3) << " / " << std::left << std::setw(20) << "Galutinis (Med.)" << std::endl;
+	cout << string(83,  '-') << std::endl;
+	cout << std::left << std::setw(20) << s.vardas << std::left << std::setw(20) << s.pavarde << std::left << std::setw(23) << std::setprecision(3) << s.v
+		<< std::left << std::setw(20) << std::setprecision(3) << s.m << std::endl;
 	
 };
+
+
+
+
+
+
+
